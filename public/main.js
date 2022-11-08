5669482b9859652e43a4d6305c8ed3f994a20fb8
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 17:52:37 -0500

    testing for error in write to p

--- public/main.js
+++ public/main.js
@@ -6,19 +6,7 @@ function run() {
       tableSection = document.getElementById("table");
       table = tableSection.getElementsByTagName("p")[0].innerText;
 
-      table = "<table border==\"1\"><tr>";
-      for (names in json[0]) {
-        table += ("<td>" + names + "</td>");
-      }
-      table += "<tr>";
-      for (var i = 0; i < json.length; i++) {
-        table += '<tr>';
-        for (names in json[i]) {
-          table += ('<td>' + json[i][names] + '</td>');
-        }
-        table += '</tr>';
-      }
-      table += "</table>";
+      table = json[0];
 
       tableSection.style.visibility = "visible";
     });
