f448ae77e70911ef2b95dc2f44e2105278e075cf
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 03:32:56 -0500

    secret

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://keem:Stormwater32@cosc484-oink.wjezri1.mongodb.net/sample_mflix?retryWrites=true&w=majority";
+const uri = "mongodb+srv://keem:<password>@cosc484-oink.wjezri1.mongodb.net/sample_mflix?retryWrites=true&w=majority";
 
 // define the first route
 app.get("/api/members", async function (req, res) {
