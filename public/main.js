3849711231d590d4a23f8a7260333032f225fd61
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 18:43:37 -0500

    +=

--- public/main.js
+++ public/main.js
@@ -5,13 +5,8 @@ function run() {
       console.log(json);
       const tableSection = document.getElementById("table");
       tableSection.style.visibility = "visible";
-      string = "";
       json.forEach(function(user) { 
-        string + user.name + ' '; 
-        console.log(user);
-        console.log(user.name)
+        tableSection.getElementsByTagName("p")[0].innerText += user;
       });
-      console.log(string);
-      tableSection.getElementsByTagName("p")[0].innerText = string;
     });
 }
