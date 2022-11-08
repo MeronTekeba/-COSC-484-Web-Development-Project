4479b209a01a0400b6bb8f6fa3a56fc08c4827f4
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 18:10:59 -0500

    no more variable

--- public/main.js
+++ public/main.js
@@ -3,7 +3,24 @@ function run() {
     .then((res) => res.json())
     .then((json) => {
       const tableSection = document.getElementById("table");
-      tableSection.getElementsByTagName("p")[0].innerText = json[0].name;
+      tableSection.getElementsByTagName("p")[0].innerText = "<table border==\"1\"><tr>";
+      
+      for (names in json[0]) {
+        tableSection.getElementsByTagName("p")[0].innerText += ("<td>" + names + "</td>");
+      }
+      
+      tableSection.getElementsByTagName("p")[0].innerText += "<tr>";
+      
+      for (var i = 0; i < json.length; i++) {
+        tableSection.getElementsByTagName("p")[0].innerText += '<tr>';
+        for (names in json[i]) {
+          tableSection.getElementsByTagName("p")[0].innerText += ('<td>' + json[i][names] + '</td>');
+        }
+        tableSection.getElementsByTagName("p")[0].innerText += '</tr>';
+      }
+      
+      tableSection.getElementsByTagName("p")[0].innerText += "</table>";
+
       tableSection.style.visibility = "visible";
     });
 }
