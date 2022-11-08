cd97b6e80e7a929e3abe8c3519a1dcf79b8fa1ea
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 18:00:52 -0500

    rename variables

--- public/main.js
+++ public/main.js
@@ -2,13 +2,9 @@ function run() {
   fetch("/api/members")
     .then((res) => res.json())
     .then((json) => {
-      console.log("First user in the array:");
-      console.log(json[0]);
-      console.log("Name of the first user in the array:");
-      console.log(json[0].name);
-
-      const detailsElement = document.getElementById("table");
-      detailsElement.getElementsByTagName("p")[0].innerText = json[0].name;
+      const tableSection = document.getElementById("table");
+      table = tableSection.getElementsByTagName("p")[0].innerText;
+      table = json[0].name;
       detailsElement.style.visibility = "visible";
     });
 }
