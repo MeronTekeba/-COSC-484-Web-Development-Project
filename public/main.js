35ebb59bc179196f61319a5cd0734eb222e325d7
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 18:14:29 -0500

    trying simple solution

--- public/main.js
+++ public/main.js
@@ -3,23 +3,10 @@ function run() {
     .then((res) => res.json())
     .then((json) => {
       const tableSection = document.getElementById("table");
-      tableSection.getElementsByTagName("p")[0].innerText = "<table border==\"1\"><tr>";
       
       for (names in json[0]) {
-        tableSection.getElementsByTagName("p")[0].innerText += ("<td>" + names + "</td>");
+        tableSection.getElementsByTagName("p")[0].innerText += names;
       }
-      
-      tableSection.getElementsByTagName("p")[0].innerText += "<tr>";
-      
-      for (var i = 0; i < json.length; i++) {
-        tableSection.getElementsByTagName("p")[0].innerText += '<tr>';
-        for (names in json[i]) {
-          tableSection.getElementsByTagName("p")[0].innerText += ('<td>' + json[i][names] + '</td>');
-        }
-        tableSection.getElementsByTagName("p")[0].innerText += '</tr>';
-      }
-      
-      tableSection.getElementsByTagName("p")[0].innerText += "</table>";
 
       tableSection.style.visibility = "visible";
     });
