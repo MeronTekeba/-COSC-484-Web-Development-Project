b5b04117cb164856bf46cfce4b3c5d8000a43688
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 18:22:36 -0500

    info

--- public/main.js
+++ public/main.js
@@ -3,10 +3,12 @@ function run() {
     .then((res) => res.json())
     .then((json) => {
       console.log(json);
+      console.log(json[0]);
+      console.log(json[0].age);
       const tableSection = document.getElementById("table");
       
       for (user in json) {
-        tableSection.getElementsByTagName("p")[0].innerText += user.name;
+        tableSection.getElementsByTagName("p")[0].innerText += user;
       }
 
       tableSection.style.visibility = "visible";
