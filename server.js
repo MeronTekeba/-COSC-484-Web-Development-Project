0eb77c76ddd4afd4852ec78220d43c1703c7680f
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 03:28:59 -0500

    updating demo sample to work with oinkdb

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const { MongoClient } = require("mongodb");
 
 const uri = "mongodb+srv://keem:Stormwater32@cosc484-oink.wjezri1.mongodb.net/sample_mflix?retryWrites=true&w=majority";
 
-// use the express-static middleware
-app.use(express.static("public"));
-
 // define the first route
 app.get("/api/members", async function (req, res) {
   const client = new MongoClient(uri, { useUnifiedTopology: true });
@@ -19,23 +16,12 @@ app.get("/api/members", async function (req, res) {
     const database = client.db('oinkdb');
     const collection = database.collection('user');
 
-    // Query for a movie that has the title 'Back to the Future'
+    // Query for users aged 21
     const query = { age: 21 };
-    const cursor = await collection.aggregate([
-      { $match: query },
-      { $sample: { size: 1 } },
-      { $project: 
-        {
-          title: 1,
-          fullplot: 1,
-          poster: 1
-        }
-      }
-    ]);
+    const cursor = collection.find(query);
+    const result = await cursor.toArray();
 
-    const movie = await cursor.next();
-
-    return res.json(movie);
+    return res.json(result);
   } catch(err) {
     console.log(err);
   }
