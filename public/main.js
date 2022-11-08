0ce132bcef8138a0d56119a14e3603220dfd85b6
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 18:34:03 -0500

    a

--- public/main.js
+++ public/main.js
@@ -3,12 +3,11 @@ function run() {
     .then((res) => res.json())
     .then((json) => {
       console.log(json);
-      console.log(json[0]);
-      console.log(json[0].age);
       const tableSection = document.getElementById("table");
-      
-      json.forEach(function(user) { tableSection.getElementsByTagName("p")[0].innerText += user.name; });
-
       tableSection.style.visibility = "visible";
+      string = "";
+      json.forEach(function(user) { string.concat(" ", user.name) });
+
+      tableSection.getElementsByTagName("p")[0].innerText = result;
     });
 }
