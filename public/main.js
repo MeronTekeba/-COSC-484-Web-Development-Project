8ab27385f4559ff3faed628659662a07457abed4
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 16:21:03 -0500

    json to html further testing

--- public/main.js
+++ public/main.js
@@ -1,11 +1,14 @@
 function run() {
   fetch("/api/members")
-    .then((response) => response.json())
-    .then((data) => {
+    .then((res) => res.json())
+    .then((json) => {
+      console.log("First user in the array:");
+      console.log(json[0]);
+      console.log("Name of the first user in the array:");
+      console.log(json[0].name);
+
       const detailsElement = document.getElementById("members");
-
-      detailsElement.getElementsByTagName("p")[0].innerText = data.name;
-
+      detailsElement.getElementsByTagName("p")[0].innerText = json[0].name;
       detailsElement.style.visibility = "visible";
     });
 }
