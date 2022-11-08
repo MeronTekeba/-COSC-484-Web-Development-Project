bef1ce58ea7b6b2ca8738bdff8b4dd4cf0d532c1
Akeem Laurence <alaure2@students.towson.edu>
2022-11-08 03:44:46 -0500

    testing why main html won't load

--- server.js
+++ server.js
@@ -6,22 +6,36 @@ const { MongoClient } = require("mongodb");
 
 const uri = process.env.MONGODB_URI;
 
+// use the express-static middleware
+app.use(express.static("public"));
+
 // define the first route
-app.get("/api/members", async function (req, res) {
+app.get("/api/movie", async function (req, res) {
   const client = new MongoClient(uri, { useUnifiedTopology: true });
   
   try {
     await client.connect();
 
-    const database = client.db('oinkdb');
-    const collection = database.collection('user');
+    const database = client.db('sample_mflix');
+    const collection = database.collection('movies');
 
-    // Query for users aged 21
-    const query = { age: 21 };
-    const cursor = collection.find(query);
-    const result = await cursor.toArray();
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { genres: "Comedy", poster: { $exists: true } };
+    const cursor = await collection.aggregate([
+      { $match: query },
+      { $sample: { size: 1 } },
+      { $project: 
+        {
+          title: 1,
+          fullplot: 1,
+          poster: 1
+        }
+      }
+    ]);
 
-    return res.json(result);
+    const movie = await cursor.next();
+
+    return res.json(movie);
   } catch(err) {
     console.log(err);
   }
