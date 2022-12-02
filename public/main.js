70c2f3d6baa6a0e7e00bed4bafd8728ed69a21d2
Akeem Laurence <alaure2@students.towson.edu>
2022-12-01 20:18:14 -0500

    css update

--- public/main.js
+++ public/main.js
@@ -10,3 +10,17 @@ function run() {
       });
     });
 }
+
+/* Verify all requirements met */
+function verifyLogin() {
+  fetch("/api/accounts")
+  .then((res) => res.json())
+  .then((json) => {
+    console.log(json);
+    const tableSection = document.getElementById("table");
+    tableSection.style.visibility = "visible";
+    json.forEach(function(user) { 
+      tableSection.getElementsByTagName("p")[0].innerText += user.name + '\n';
+    });
+  });
+}
--- server.js
+++ server.js
@@ -9,6 +9,31 @@ const uri = process.env.MONGODB_URI;
 // use the express-static middleware
 app.use(express.static("public"));
 
+// login
+app.get("/api/login", async function (req, res) {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  console.log(req);
+  try {
+    await client.connect();
+
+    const database = client.db('oinkdb');
+    const collection = database.collection('accounts');
+
+    const query = req.query;
+    const cursor = collection.find(query);
+    const result = await cursor.toArray();
+
+    return res.json(result);
+    
+  } catch(err) {
+    console.log(err);
+  }
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+});
+
 // query the accounts db
 app.get("/api/accounts", async function (req, res) {
   const client = new MongoClient(uri, { useUnifiedTopology: true });
