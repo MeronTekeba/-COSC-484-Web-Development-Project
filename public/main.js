2dc09e8447f5e97773ddddfc31ea4a85c97ded14
Akeem Laurence <alaure2@students.towson.edu>
2022-12-03 03:30:47 -0500

    login layout changes and sign up layout

--- public/main.js
+++ public/main.js
@@ -18,10 +18,38 @@ function verifyLogin() {
   .then((json) => {
     alert(JSON.stringify(json))
     if(JSON.stringify(json) == "[]") {
-      document.getElementById("fail").innerHTML = "incorrect";
-      alert("hey, it empty");
+      document.getElementById("fail").innerHTML = "incorrect email address or password";
+      return false;
     }
-    alert("hey, it good");
   });
-  return false;
+  return true;
+}
+
+function verifyNewAccount() {
+  return verifyUsername() && verifyPassword()
+}
+
+/* Verify Username Length
+   - Length L must satisfy 5 < L < 24 */
+   function verifyUsername() {
+    var len = document.getElementById('floatingUsername').value.length;
+    if( len < 6 ) {
+        alert('username must be at least 6 characters long');
+        return false;
+    }
+    else if( len > 24 ) {
+        alert('username must be less than 24 characters long');
+        return false;
+    }
+    return true;
+}
+
+/* Verify Password Match
+   - Password and Confirm Password Inputs must match */
+function verifyPassword() {
+    if(document.getElementById('floatingPassword').value != document.getElementById('floatingConfirmPassword').value) {
+        alert('ensure passwords match');
+        return false;
+    }
+    return true;
 }
